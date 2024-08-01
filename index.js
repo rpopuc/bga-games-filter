dfdcb493fae10a7ece4de0a0054eb4516bd55f5c
Robson P Oliveira <rpopuc@gmail.com>
2024-08-01 16:36:23 -0300

    chore: rename folder dist to public

--- index.js
+++ index.js
@@ -80,10 +80,10 @@ io.on('connection', (socket) => {
   }
 });
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 server.listen(3000, () => {
